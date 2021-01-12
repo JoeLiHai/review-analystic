872ff7e5de93148c5827c2a3e89251f713e28481
Joe <joelihai2021@gmail.com>
2021-01-12 15:33:16 +0800

    final_version

--- review.py
+++ review.py
@@ -4,9 +4,19 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 ==0:
+		if count % 100000 ==0:
 			print(len(data))
-print(len(data))
-print(data[0])
-print('---------------------')
-print(data[1])
+print('檔案讀取完成, 總共有', len(data), '筆資料')
+
+sum = 0
+for i in range(0, len(data)):
+	sum += len(data[i])
+print('留言的平均長度為: ', sum / len(data))
+
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('老師版留言的平均長度為', sum_len / len(data))
+
+
