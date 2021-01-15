b9a726b157e3751091147c22fbe26ac048b0edd0
JoeLiHai <77216127+JoeLiHai@users.noreply.github.com>
2021-01-15 14:24:14 +0800

    add count function

--- review.py
+++ review.py
@@ -8,27 +8,60 @@ with open('reviews.txt', 'r') as f:
 			print(len(data))
 print('檔案讀取完成, 總共有', len(data), '筆資料')
 
-sum = 0
-for i in range(0, len(data)):
-	sum += len(data[i])
-print('留言的平均長度為: ', sum / len(data))
+print(data[0])
 
-
-sum_len = 0
+wc = {}  # word_count
 for d in data:
-	sum_len += len(d)
-print('老師版留言的平均長度為', sum_len / len(data))
+	words = d.split(' ')
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1
 
-new = []
-for d in data:
-	if len(d) < 100:
-		new.append(d)
-print('一共有', len(new), '筆留言長度小於100')
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+print(len(wc))
 
-# list comprehension 清單快寫法
+while True:
+	word = input('請問你想查什麼字: ')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現過的次數為', wc[word])
+	else:
+		print('沒有這個字')
 
-good = [d for d in data if 'good' in d]         
-print(good)
 
-bad = ['bad' in d for d in data]
-# 留言有提到bad才會是True
+
+
+
+
+
+
+
+
+# sum = 0
+# for i in range(0, len(data)):
+# 	sum += len(data[i])
+# print('留言的平均長度為: ', sum / len(data))
+
+
+# sum_len = 0
+# for d in data:
+# 	sum_len += len(d)
+# print('老師版留言的平均長度為', sum_len / len(data))
+
+# new = []
+# for d in data:
+# 	if len(d) < 100:
+# 		new.append(d)
+# print('一共有', len(new), '筆留言長度小於100')
+
+# # list comprehension 清單快寫法
+
+# good = [d for d in data if 'good' in d]         
+# print(good)
+
+# bad = ['bad' in d for d in data] # 留言有提到bad才會是True
